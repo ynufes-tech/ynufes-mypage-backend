02b1c893cd87ff3d5f86c8d92e26fe02d5ef7809
Shion Ichikawa <shion1305@gmail.com>
2022-11-08 11:11:25 +0900

    ✨ implement line state manager(authentication)

---
+++ line_state_manager.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+var stateCache map[string]int64
+
+func reqState(c *gin.Context) {
+	var newState string
+	newState = strconv.FormatUint(rand.Uint64(), 10)
+	for _, duplicate := stateCache[newState]; duplicate; {
+		newState = strconv.FormatUint(rand.Uint64(), 10)
+	}
+	c.Writer.WriteString(newState)
+	stateCache[newState] = time.Now().Unix()
+	go revokeOldStates()
+}
+
+func verifyState(entry string) bool {
+	r, res := stateCache[entry]
+	if !res {
+		return false
+	}
+	delete(stateCache, entry)
+	if time.Now().Unix()-r > 120000 {
+		return false
+	}
+	return true
+}
+
+func revokeOldStates() {
+	for s, t := range stateCache {
+		if time.Now().Unix()-t > 120000 {
+			delete(stateCache, s)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/hello/", testHello)
 	router.GET("/auth/line/callback", lineCallback)
-
+	router.GET("/auth/line/reqState", reqState)
 	err := router.Run("localhost:1306")
 	if err != nil {
 		fmt.Println("Failed to start server...")
