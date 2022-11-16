d99f88975e1771eacda6e7cfb1a7b42b3ca008d4
Shion Ichikawa <shion1305@gmail.com>
2022-11-17 04:50:48 +0900

    ✅ Writing test OAuth code

--- gin_handlers.go
+++ gin_handlers.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"strconv"
+	"ynufes-mypage-backend/pkg/line"
+)
 
 func testHello(c *gin.Context) {
 	name := c.Request.URL.Path[len("/hello/"):]
@@ -8,5 +12,22 @@ func testHello(c *gin.Context) {
 }
 
 func lineCallback(c *gin.Context) {
+	code := c.Request.URL.Query().Get("code")
+	state := c.Request.URL.Query().Get("state")
+	if !line.VerifyState(state) {
+		c.Status(401)
+		c.Writer.WriteString("Your request is not valid...")
+		return
+	}
+	accessResponse, err := line.RequestAccessToken(code)
+	if err != nil {
+		c.Writer.WriteString("Error, " + err.Error())
+	} else {
+		c.Writer.WriteString("AccessToken: " + accessResponse.AccessToken + "\n")
+		c.Writer.WriteString("TokenType: " + accessResponse.TokenType + "\n")
+		c.Writer.WriteString("ExpiresIn: " + strconv.FormatInt(accessResponse.ExpiresIn, 10) + "\n")
+		c.Writer.WriteString("RefreshToken: " + accessResponse.RefreshToken + "\n")
+		c.Writer.WriteString("Scope: " + accessResponse.Scope + "\n")
+	}
 
 }
