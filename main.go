6f93d2631b15c8875b4772f3627cea551404a091
Shion Ichikawa <shion1305@gmail.com>
2022-11-17 05:32:19 +0900

    ✨ make .env read from main

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	loadEnv()
 	router := gin.Default()
 	router.GET("/hello/", testHello)
 	router.GET("/auth/line/callback", lineCallback)
@@ -14,4 +16,8 @@ func main() {
 	if err != nil {
 		fmt.Println("Failed to start server...")
 	}
+}
+
+func loadEnv() {
+	godotenv.Load()
 }
