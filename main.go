91809cf76079e51fd8c0d0a958e8e8098a517b45
Shion Ichikawa <shion1305@gmail.com>
2022-11-17 06:01:46 +0900

    ✅ add test endpoint for development (oauth test)

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"os"
 	"ynufes-mypage-backend/pkg/line"
 )
 
@@ -13,7 +14,7 @@ func main() {
 	router.GET("/hello/", testHello)
 	router.GET("/auth/line/callback", lineCallback)
 	router.GET("/auth/line/reqState", line.ReqState)
-	//router.GET("auth/line/dev/auth", devAuth)
+	router.GET("auth/line/dev/auth", devAuth)
 	err := router.Run("localhost:1306")
 	if err != nil {
 		fmt.Println("Failed to start server...")
@@ -22,4 +23,9 @@ func main() {
 
 func loadEnv() {
 	godotenv.Load()
+}
+
+func devAuth(c *gin.Context) {
+	c.Redirect(502, "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id="+os.Getenv(line.EnvLineClientId)+
+		"&redirect_uri="+os.Getenv(line.EnvLineRedirectUri)+"&state="+line.IssueNewState()+"&scope=openid%20profile%20email")
 }
