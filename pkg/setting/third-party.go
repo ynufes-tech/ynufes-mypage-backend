4437eabfbe747cccd1249ec4e5acfbb02ceb6265
Shion Ichikawa <shion1305@gmail.com>
2022-12-12 21:19:11 +0900

    ✨ LineLoginの設定フィールドをpkg/settingに追加

--- pkg/setting/third-party.go
+++ pkg/setting/third-party.go
@@ -2,5 +2,12 @@ package setting
 
 type (
 	ThirdParty struct {
+		LineLogin LineLogin `yaml:"line_login"`
+	}
+
+	LineLogin struct {
+		ClientID     string `yaml:"client_id"`
+		ClientSecret string `yaml:"client_secret"`
+		CallbackURI  string `yaml:"callback_uri"`
 	}
 )
