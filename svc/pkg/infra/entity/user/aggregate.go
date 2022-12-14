d80285f40c7990d79de99c8bc2f48284c3be9ff9
Shion Ichikawa <shion1305@gmail.com>
2022-12-14 17:05:36 +0900

    ✨ (infra/entity/user) ToModelの実装

--- svc/pkg/infra/entity/user/aggregate.go
+++ svc/pkg/infra/entity/user/aggregate.go
@@ -1,6 +1,36 @@
 package entity
 
+import "ynufes-mypage-backend/svc/pkg/domain/model/user"
+
 type User struct {
+	ID int64
 	UserDetail
 	LineAuthorization
+}
+
+func (u User) ToModel() (*user.User, error) {
+	email, err := user.NewEmail(u.Email)
+	if err != nil {
+		return nil, err
+	}
+	gender, err := user.NewGender(u.Gender)
+	if err != nil {
+		return nil, err
+	}
+	return &user.User{
+		ID: user.ID(u.ID),
+		Detail: user.Detail{
+			Name: user.Name{
+				FirstName:     u.NameFirst,
+				LastName:      u.NameLast,
+				FirstNameKana: u.NameFirstKana,
+				LastNameKana:  u.NameLastKana,
+			},
+			Email:  email,
+			Gender: gender,
+			// TODO: add validation for StudentID, Type
+			StudentID: user.StudentID(u.StudentID),
+			Type:      user.Type(u.Type),
+		},
+	}, nil
 }
