75b94ed3a5920e82022f076aed170722cdba1417
Shion Ichikawa <shion1305@gmail.com>
2022-12-14 15:21:42 +0900

    ✨ (pkg/setting) JWTSecretフィールドの追加

--- pkg/setting/application.go
+++ pkg/setting/application.go
@@ -3,8 +3,12 @@ package setting
 type (
 	Application struct {
 		Server Server `yaml:"server"`
+		Admin  Admin  `yaml:"admin"`
 	}
 	Server struct {
 		Domain string `yaml:"domain"`
+	}
+	Admin struct {
+		JwtSecret string `yaml:"jwt_secret"`
 	}
 )
