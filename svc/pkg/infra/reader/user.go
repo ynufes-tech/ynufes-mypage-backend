83a84a1101372184367d28b4743088c4cc79f7f4
Shion Ichikawa <shion1305@gmail.com>
2022-12-14 17:11:17 +0900

    ✨ (infra/reader) userのreaderを実装

---
+++ svc/pkg/infra/reader/user.go
@@ -0,0 +1,42 @@
+package reader
+
+import (
+	"cloud.google.com/go/firestore"
+	"context"
+	"strconv"
+	"ynufes-mypage-backend/svc/pkg/domain/model/user"
+	entity "ynufes-mypage-backend/svc/pkg/infra/entity/user"
+)
+
+const (
+	UserCollectionName = "users"
+)
+
+type (
+	User struct {
+		Collection *firestore.CollectionRef
+	}
+)
+
+func NewUser(c *firestore.CollectionRef) User {
+	return User{
+		c,
+	}
+}
+
+func (u User) Get(ctx context.Context, id user.ID) (model *user.User, err error) {
+	var userEntity entity.User
+	snap, err := u.Collection.Doc(strconv.FormatInt(int64(id), 10)).Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	err = snap.DataTo(&userEntity)
+	if err != nil {
+		return nil, err
+	}
+	model, err = userEntity.ToModel()
+	if err != nil {
+		return nil, err
+	}
+	return model, nil
+}
