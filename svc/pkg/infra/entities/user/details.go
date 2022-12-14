ef129f9de414466663af030edac81c5438d81b67
Shion Ichikawa <shion1305@gmail.com>
2022-12-14 15:24:01 +0900

    👽️ (infra/entities) AdmissionYear -> StudentID

--- svc/pkg/infra/entities/user/details.go
+++ svc/pkg/infra/entities/user/details.go
@@ -1,12 +1,13 @@
 package entities
 
 type (
+	// UserDetail StudentID 学部生は数字7桁, 大学院生は数字2桁+英数字2桁+数字3桁
 	UserDetail struct {
 		NameFirst     string `firestore:"name_first"`
 		NameFirstKana string `firestore:"name_first_kana"`
 		NameLast      string `firestore:"name_last"`
 		NameLastKana  string `firestore:"name_last_kana"`
 		Gender        int    `firestore:"gender"`
-		AdmissionYear int    `firestore:"admission_year"`
+		StudentID     string `firestore:"student_id"`
 	}
 )
