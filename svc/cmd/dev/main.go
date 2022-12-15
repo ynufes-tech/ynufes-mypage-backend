1d47200131162c7d467d1fd8e97a0b4cc6d3d6c3
Shion Ichikawa <shion1305@gmail.com>
2022-12-16 05:46:08 +0900

    🔥 無効なコードをコメント化

--- svc/cmd/dev/main.go
+++ svc/cmd/dev/main.go
@@ -1,36 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"net/url"
-	"os"
-	linePkg "ynufes-mypage-backend/pkg/line"
-	"ynufes-mypage-backend/pkg/setting"
-	lineHandler "ynufes-mypage-backend/svc/handler/line"
-	"ynufes-mypage-backend/svc/handler/test"
 )
 
 func main() {
 	loadEnv()
-	router := gin.Default()
-	router.GET("/hello/", test.TestHello)
-	router.GET("/auth/lineHandler/callback", lineHandler.Callback)
-	router.GET("/auth/lineHandler/reqState", linePkg.ReqState)
-	router.GET("/auth/lineHandler/dev/auth", devAuth)
-	err := router.Run("localhost:1306")
-	if err != nil {
-		fmt.Println("Failed to start server...")
-	}
+	//router := gin.Default()
+	//router.GET("/hello/", test.TestHello)
+	//router.GET("/auth/lineHandler/callback", lineHandler.Callback)
+	//router.GET("/auth/lineHandler/reqState", linePkg.ReqState)
+	//router.GET("/auth/lineHandler/dev/auth", devAuth)
+	//err := router.Run("localhost:1306")
+	//if err != nil {
+	//	fmt.Println("Failed to start server...")
+	//}
 }
 
 func loadEnv() {
 	godotenv.Load()
 }
 
-func devAuth(c *gin.Context) {
-	config := setting.Get()
-	c.Redirect(302, "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id="+os.Getenv(config.ThirdParty.LineLogin.ClientID)+
-		"&redirect_uri="+url.QueryEscape(os.Getenv(config.ThirdParty.LineLogin.CallbackURI))+"&state="+linePkg.IssueNewState()+"&scope=openid%20profile%20email")
-}
+//func devAuth(c *gin.Context) {
+//	config := setting.Get()
+//	c.Redirect(302, "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id="+os.Getenv(config.ThirdParty.LineLogin.ClientID)+
+//		"&redirect_uri="+url.QueryEscape(os.Getenv(config.ThirdParty.LineLogin.CallbackURI))+"&state="+linePkg.IssueNewState()+"&scope=openid%20profile%20email")
+//}
--- svc/handler/line/handler.go
+++ svc/handler/line/handler.go
@@ -1,30 +1,30 @@
 package line
 
-import (
-	"github.com/gin-gonic/gin"
-	"strconv"
-	linePkg "ynufes-mypage-backend/pkg/line"
-	"ynufes-mypage-backend/pkg/setting"
-)
-
-func Callback(c *gin.Context) {
-	config := setting.Get()
-	code := c.Request.URL.Query().Get("code")
-	state := c.Request.URL.Query().Get("state")
-	if !linePkg.VerifyState(state) {
-		c.Status(401)
-		c.Writer.WriteString("Your request is not valid...")
-		return
-	}
-	accessResponse, err := linePkg.RequestAccessToken(code)
-	if err != nil {
-		c.Writer.WriteString("Error, " + err.Error())
-	} else {
-		c.Writer.WriteString("AccessToken: " + accessResponse.AccessToken + "\n")
-		c.Writer.WriteString("TokenType: " + accessResponse.TokenType + "\n")
-		c.Writer.WriteString("ExpiresIn: " + strconv.FormatInt(accessResponse.ExpiresIn, 10) + "\n")
-		c.Writer.WriteString("RefreshToken: " + accessResponse.RefreshToken + "\n")
-		c.Writer.WriteString("Scope: " + accessResponse.Scope + "\n")
-	}
-	c.SetCookie("access_token", accessResponse.AccessToken, 3600, "/", config.Application.Server.Domain, true, true)
-}
+//import (
+//	"github.com/gin-gonic/gin"
+//	"strconv"
+//	linePkg "ynufes-mypage-backend/pkg/line"
+//	"ynufes-mypage-backend/pkg/setting"
+//)
+//
+//func Callback(c *gin.Context) {
+//	config := setting.Get()
+//	code := c.Request.URL.Query().Get("code")
+//	state := c.Request.URL.Query().Get("state")
+//	if !linePkg.VerifyState(state) {
+//		c.Status(401)
+//		c.Writer.WriteString("Your request is not valid...")
+//		return
+//	}
+//	accessResponse, err := linePkg.RequestAccessToken(code)
+//	if err != nil {
+//		c.Writer.WriteString("Error, " + err.Error())
+//	} else {
+//		c.Writer.WriteString("AccessToken: " + accessResponse.AccessToken + "\n")
+//		c.Writer.WriteString("TokenType: " + accessResponse.TokenType + "\n")
+//		c.Writer.WriteString("ExpiresIn: " + strconv.FormatInt(accessResponse.ExpiresIn, 10) + "\n")
+//		c.Writer.WriteString("RefreshToken: " + accessResponse.RefreshToken + "\n")
+//		c.Writer.WriteString("Scope: " + accessResponse.Scope + "\n")
+//	}
+//	c.SetCookie("access_token", accessResponse.AccessToken, 3600, "/", config.Application.Server.Domain, true, true)
+//}
