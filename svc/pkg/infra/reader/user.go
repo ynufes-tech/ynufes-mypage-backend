90659f239d88cf4c62f6d5f9270719f2513adbb2
Shion Ichikawa <shion1305@gmail.com>
2022-12-17 05:22:55 +0900

    ✨ (infra/reader) GetByLineServiceID

--- svc/pkg/infra/reader/user.go
+++ svc/pkg/infra/reader/user.go
@@ -24,9 +24,26 @@ func NewUser(c *firestore.CollectionRef) User {
 	}
 }
 
-func (u User) Get(ctx context.Context, id user.ID) (model *user.User, err error) {
+func (u User) GetByID(ctx context.Context, id user.ID) (model *user.User, err error) {
 	var userEntity entity.User
 	snap, err := u.Collection.Doc(strconv.FormatInt(int64(id), 10)).Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	err = snap.DataTo(&userEntity)
+	if err != nil {
+		return nil, err
+	}
+	model, err = userEntity.ToModel()
+	if err != nil {
+		return nil, err
+	}
+	return model, nil
+}
+
+func (u User) GetByLineServiceID(ctx context.Context, id user.LineServiceID) (model *user.User, err error) {
+	var userEntity entity.User
+	snap, err := u.Collection.Where("line-id", "==", string(id)).Documents(ctx).Next()
 	if err != nil {
 		return nil, err
 	}
