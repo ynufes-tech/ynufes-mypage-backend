6bf9df69c11b213f1c40e92b430839f3f45daded
Shion Ichikawa <shion1305@gmail.com>
2022-12-17 02:16:56 +0900

    ✅ テストケースの追加

--- svc/pkg/infra/users_test.go
+++ svc/pkg/infra/users_test.go
@@ -4,28 +4,53 @@ import (
 	"ynufes-mypage-backend/svc/pkg/domain/model/user"
 )
 
-func genTestCase() user.User {
-	return user.User{
-		ID: 1234,
-		Detail: user.Detail{
-			Name: user.Name{
-				FirstName:     "詩恩",
-				LastName:      "市川",
-				FirstNameKana: "シオン",
-				LastNameKana:  "イチカワ",
+func genTestCase() []user.User {
+	return []user.User{
+		{
+			ID: 1234,
+			Detail: user.Detail{
+				Name: user.Name{
+					FirstName:     "詩恩",
+					LastName:      "市川",
+					FirstNameKana: "シオン",
+					LastNameKana:  "イチカワ",
+				},
+				Email:     "shion1305@gmail.com",
+				Gender:    user.GenderMan,
+				StudentID: "2164027",
+				Type:      user.TypeNormal,
 			},
-			Email:     "shion1305@gmail.com",
-			Gender:    user.GenderMan,
-			StudentID: "2164027",
-			Type:      user.TypeNormal,
+			Line: user.Line{
+				LineServiceID:         "LineServiceID",
+				EncryptedAccessToken:  "EncryptedAccessToken",
+				EncryptedRefreshToken: "EncryptedRefreshToken",
+			},
+			Dashboard: user.Dashboard{
+				Grants: []string{"grant1", "grant2"},
+			},
 		},
-		Line: user.Line{
-			LineServiceID:         "LineServiceID",
-			EncryptedAccessToken:  "EncryptedAccessToken",
-			EncryptedRefreshToken: "EncryptedRefreshToken",
-		},
-		Dashboard: user.Dashboard{
-			Grants: []string{"grant1", "grant2"},
+		{
+			ID: 1234,
+			Detail: user.Detail{
+				Name: user.Name{
+					FirstName:     "友哉",
+					LastName:      "廣江",
+					FirstNameKana: "トモヤ",
+					LastNameKana:  "ヒロエ",
+				},
+				Email:     "tomoya4creative@gmail.com",
+				Gender:    user.GenderMan,
+				StudentID: "2125178",
+				Type:      user.TypeNormal,
+			},
+			Line: user.Line{
+				LineServiceID:         "LineServiceID",
+				EncryptedAccessToken:  "EncryptedAccessToken",
+				EncryptedRefreshToken: "EncryptedRefreshToken",
+			},
+			Dashboard: user.Dashboard{
+				Grants: []string{"grant1", "grant2"},
+			},
 		},
 	}
 }
