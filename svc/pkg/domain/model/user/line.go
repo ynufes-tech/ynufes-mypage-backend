3548623b9602b8c8c3cc35a395523d3455368cd8
Shion Ichikawa <shion1305@gmail.com>
2022-12-17 19:07:37 +0900

    ✨ LineのAccessToken暗号管理メゾットの追加

--- svc/pkg/domain/model/user/line.go
+++ svc/pkg/domain/model/user/line.go
@@ -1,12 +1,56 @@
 package user
 
+import (
+	"ynufes-mypage-backend/pkg/cipher"
+	"ynufes-mypage-backend/pkg/setting"
+)
+
 type (
 	LineServiceID         string
 	EncryptedAccessToken  string
 	EncryptedRefreshToken string
+	PlainAccessToken      string
+	PlainRefreshToken     string
 	Line                  struct {
 		LineServiceID         LineServiceID
 		EncryptedAccessToken  EncryptedAccessToken
 		EncryptedRefreshToken EncryptedRefreshToken
 	}
 )
+
+var aes *cipher.AES
+
+func init() {
+	config := setting.Get()
+	aes, _ = cipher.NewAES(config.ThirdParty.LineLogin.CipherKey)
+}
+
+func NewLineServiceID(s string) (LineServiceID, error) {
+	return LineServiceID(s), nil
+}
+
+func NewEncryptedAccessToken(s PlainAccessToken) (EncryptedAccessToken, error) {
+	encrypted := aes.Encrypt(string(s))
+	return EncryptedAccessToken(encrypted), nil
+}
+
+func NewEncryptedRefreshToken(s PlainRefreshToken) (EncryptedRefreshToken, error) {
+	encrypted := aes.Encrypt(string(s))
+	return EncryptedRefreshToken(encrypted), nil
+}
+
+func (l Line) AccessToken() (PlainAccessToken, error) {
+	decrypted, err := aes.Decrypt(string(l.EncryptedAccessToken))
+	if err != nil {
+		return PlainAccessToken(""), err
+	}
+	return PlainAccessToken(decrypted), nil
+}
+
+func (l Line) RefreshToken() (PlainRefreshToken, error) {
+	decrypted, err := aes.Decrypt(string(l.EncryptedRefreshToken))
+	if err != nil {
+		return PlainRefreshToken(""), err
+	}
+	return PlainRefreshToken(decrypted), nil
+}
