bfad39ea882d9f082ddc39d4858d2719e402c691
Shion Ichikawa <shion1305@gmail.com>
2022-12-18 03:57:44 +0900

    ✨ (pkg)JWTのVerifyメゾット

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,10 +1,13 @@
 package jwt
 
-import "github.com/golang-jwt/jwt"
+import (
+	"errors"
+	"github.com/golang-jwt/jwt"
+)
 
 type JWT string
 
-func NewJWT(claims jwt.Claims, secret string) (JWT, error) {
+func IssueJWT(claims jwt.Claims, secret string) (JWT, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	tokenString, err := token.SignedString([]byte(secret))
 	if err != nil {
@@ -15,4 +18,22 @@ func NewJWT(claims jwt.Claims, secret string) (JWT, error) {
 
 func (j JWT) String() string {
 	return string(j)
+}
+
+var ErrInvalidJWT = errors.New("INVALID JWT")
+
+func (j JWT) Verify(secret string) (jwt.Claims, error) {
+	token, err := jwt.ParseWithClaims(string(j), jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return "", errors.New("UNEXPECTED SIGNING METHOD")
+		}
+		if token.Valid {
+			return nil, ErrInvalidJWT
+		}
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token.Claims, nil
 }
