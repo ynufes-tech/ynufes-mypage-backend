bbfd68bf886889706323a8ca3284ab4f5af7a800
Shion Ichikawa <shion1305@gmail.com>
2022-12-18 23:32:03 +0900

    ✨ (middleware)auth

---
+++ svc/pkg/middleware/auth.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"ynufes-mypage-backend/svc/pkg/domain/model/user"
+	"ynufes-mypage-backend/svc/pkg/exception"
+	"ynufes-mypage-backend/svc/pkg/registry"
+	uc "ynufes-mypage-backend/svc/pkg/uc/user"
+)
+
+const (
+	AuthorizedUserIDField = "AuthorizedUserID"
+	UserContextKey        = "User"
+)
+
+type auth struct {
+	loginUC uc.LoginUseCase
+}
+
+func NewAuth(registry registry.Registry) auth {
+	return auth{
+		loginUC: uc.NewLoginUseCase(registry),
+	}
+}
+
+func (a auth) VerifyUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		jwt, err := getJWTFromHeader(c)
+		if err != nil {
+			c.AbortWithStatusJSON(400, gin.H{"error": err.Error()})
+			return
+		}
+		result, err := a.loginUC.Do(c, uc.LoginInput{JWT: user.JWT(jwt)})
+		c.Set(UserContextKey, result)
+		if err != nil {
+			c.AbortWithError(401, err)
+		}
+		c.Set(AuthorizedUserIDField, result.User.ID)
+		c.Next()
+	}
+}
+
+func getJWTFromHeader(c *gin.Context) (string, error) {
+	header := c.GetHeader("Authorization")
+
+	if header[:7] != "Bearer " {
+		return "", exception.ErrorInvalidHeader
+	}
+	return header[7:], nil
+}
