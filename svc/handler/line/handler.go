6c4ed344bdb49e672da07eb9944a08455d4204c0
Shion Ichikawa <shion1305@gmail.com>
2022-12-19 00:55:53 +0900

    ✨ (handler)line auth handler

--- svc/handler/line/handler.go
+++ svc/handler/line/handler.go
@@ -1,30 +1,43 @@
 package line
 
-//import (
-//	"github.com/gin-gonic/gin"
-//	"strconv"
-//	linePkg "ynufes-mypage-backend/pkg/line"
-//	"ynufes-mypage-backend/pkg/setting"
-//)
-//
-//func Callback(c *gin.Context) {
-//	config := setting.Get()
-//	code := c.Request.URL.Query().Get("code")
-//	state := c.Request.URL.Query().Get("state")
-//	if !linePkg.VerifyState(state) {
-//		c.Status(401)
-//		c.Writer.WriteString("Your request is not valid...")
-//		return
-//	}
-//	accessResponse, err := linePkg.RequestAccessToken(code)
-//	if err != nil {
-//		c.Writer.WriteString("Error, " + err.Error())
-//	} else {
-//		c.Writer.WriteString("AccessToken: " + accessResponse.AccessToken + "\n")
-//		c.Writer.WriteString("TokenType: " + accessResponse.TokenType + "\n")
-//		c.Writer.WriteString("ExpiresIn: " + strconv.FormatInt(accessResponse.ExpiresIn, 10) + "\n")
-//		c.Writer.WriteString("RefreshToken: " + accessResponse.RefreshToken + "\n")
-//		c.Writer.WriteString("Scope: " + accessResponse.Scope + "\n")
-//	}
-//	c.SetCookie("access_token", accessResponse.AccessToken, 3600, "/", config.Application.Server.Domain, true, true)
-//}
+import (
+	"github.com/gin-gonic/gin"
+	linePkg "ynufes-mypage-backend/pkg/line"
+	"ynufes-mypage-backend/pkg/setting"
+	"ynufes-mypage-backend/svc/pkg/domain/query"
+	lineDomain "ynufes-mypage-backend/svc/pkg/domain/service/line"
+)
+
+type LineAuth struct {
+	verifier lineDomain.AuthVerifier
+	query    query.User
+}
+
+func NewLineAuth() LineAuth {
+	config := setting.Get()
+	return LineAuth{
+		verifier: linePkg.NewAuthVerifier(
+			config.ThirdParty.LineLogin.CallbackURI,
+			config.ThirdParty.LineLogin.ClientID,
+			config.ThirdParty.LineLogin.ClientSecret),
+	}
+}
+
+func (a LineAuth) VerificationHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		code := c.Request.URL.Query().Get("code")
+		state := c.Request.URL.Query().Get("state")
+		_, err := a.verifier.RequestAccessToken(code, state)
+		if err != nil {
+			return
+		}
+	}
+}
+
+func (a LineAuth) StateIssuer() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		state := a.verifier.IssueNewState()
+		_, _ = c.Writer.WriteString(state)
+		c.Status(200)
+	}
+}
