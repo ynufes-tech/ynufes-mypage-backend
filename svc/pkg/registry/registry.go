45acd6a9e7c4e614367364c206dad0b858c7a799
Shion Ichikawa <shion1305@gmail.com>
2022-12-19 01:35:07 +0900

    ✨ (registry)RegistryにRepositoryの追加

--- svc/pkg/registry/registry.go
+++ svc/pkg/registry/registry.go
@@ -1,10 +1,14 @@
 package registry
 
 type Registry struct {
-	//repo Repository
-	svc Service
+	repo Repository
+	svc  Service
 }
 
 func (r Registry) Service() Service {
 	return r.svc
+}
+
+func (r Registry) Repository() Repository {
+	return r.repo
 }
