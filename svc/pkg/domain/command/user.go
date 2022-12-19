4e8c4a9b6311a394f1ed91adbc89a332f376ffb4
Shion Ichikawa <shion1305@gmail.com>
2022-12-20 06:14:02 +0900

    🎨 (command)UpdateLineAuth -> UpdateLine

--- svc/pkg/domain/command/user.go
+++ svc/pkg/domain/command/user.go
@@ -7,7 +7,7 @@ import (
 
 type User interface {
 	Create(context.Context, user.User) error
-	UpdateLineAuth(context.Context, user.User) error
+	UpdateLine(context.Context, user.User) error
 	UpdateAll(context.Context, user.User) error
 	Delete(context.Context, user.User) error
 }
