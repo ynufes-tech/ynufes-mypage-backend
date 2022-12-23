f5bfc45d70e437ca247b56efad9f6593e5c7cfc8
Shion Ichikawa <shion1305@gmail.com>
2022-12-24 04:05:50 +0900

    🐛 (pkg)Firestore Client初期化タイミングの変更

--- pkg/firestore/client.go
+++ pkg/firestore/client.go
@@ -12,17 +12,16 @@ var (
 	client *firestore.Client
 )
 
-func init() {
-	ctx := context.Background()
-	config := setting.Get()
-	data, err := os.ReadFile(config.Infrastructure.Firestore.JsonCredentialFile)
-	options := option.WithCredentialsJSON(data)
-	client, err = firestore.NewClient(ctx, config.Infrastructure.Firestore.ProjectID, options)
-	if err != nil {
-		panic(err)
-	}
-}
-
 func New() *firestore.Client {
+	if client != nil {
+		ctx := context.Background()
+		config := setting.Get()
+		data, err := os.ReadFile(config.Infrastructure.Firestore.JsonCredentialFile)
+		options := option.WithCredentialsJSON(data)
+		client, err = firestore.NewClient(ctx, config.Infrastructure.Firestore.ProjectID, options)
+		if err != nil {
+			panic(err)
+		}
+	}
 	return client
 }
