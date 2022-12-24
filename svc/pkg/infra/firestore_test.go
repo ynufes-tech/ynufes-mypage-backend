d515aff4b1398ac1b5b6648147600a8153b5e69d
Shion Ichikawa <shion1305@gmail.com>
2022-12-25 08:00:57 +0900

    ✅ firestore_test.goをtestutilに対応

--- svc/pkg/infra/firestore_test.go
+++ svc/pkg/infra/firestore_test.go
@@ -4,25 +4,19 @@ import (
 	"context"
 	"fmt"
 	"github.com/go-playground/assert/v2"
-	"io"
-	"log"
-	"os"
-	"os/exec"
-	"runtime"
-	"strings"
-	"sync"
-	"syscall"
 	"testing"
+	"ynufes-mypage-backend/pkg/testutil"
 	"ynufes-mypage-backend/svc/pkg/infra/reader"
 	"ynufes-mypage-backend/svc/pkg/infra/writer"
 
 	"cloud.google.com/go/firestore"
 )
 
+var client *firestore.Client
+
 func TestFirestore(t *testing.T) {
-	client := newFirestoreTestClient(context.Background())
 	w := writer.NewUser(client)
-	test1 := genTestCase()
+	test1 := testutil.Users()
 	err := w.Create(context.Background(), test1[0])
 	if err != nil {
 		t.Fatal(err)
@@ -37,98 +31,11 @@ func TestFirestore(t *testing.T) {
 	fmt.Println(u)
 }
 
-func newFirestoreTestClient(ctx context.Context) *firestore.Client {
-	client, err := firestore.NewClient(ctx, "ynufes-mypage")
-	if err != nil {
-		log.Fatalf("firebase.NewClient err: %v", err)
-	}
-
-	return client
-}
-
 func TestMain(m *testing.M) {
-	// noinspection
-	var result int
-	if runtime.GOOS == "windows" {
-		killer := launchEmulatorOnWindows()
-		defer killer(&result)
+	c, killer := testutil.NewFirestoreTestClient(context.Background())
+	client = c
+	if killer != nil {
+		defer killer()
 	}
-	// now it's running, we can run our unit tests
-	result = m.Run()
+	m.Run()
 }
-
-// refer this page for following code.
-// https://www.captaincodeman.com/unit-testing-with-firestore-emulator-and-go
-func launchEmulatorOnWindows() (killer func(result *int)) {
-	// command to start firestore emulator
-	cmd := exec.Command("gcloud", "beta", "emulators", "firestore", "start", "--host-port=localhost")
-	// this makes it killable
-	cmd.SysProcAttr = &syscall.SysProcAttr{}
-
-	// we need to capture it's output to know when it's started
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stderr.Close()
-
-	if err := cmd.Start(); err != nil {
-		log.Fatal(err)
-	}
-	// we're going to wait until it's running to start
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	// by starting a separate go routine
-	go func() {
-		// reading it's output
-		buf := make([]byte, 256, 256)
-		for {
-			n, err := stderr.Read(buf[:])
-			if err != nil {
-				// until it ends
-				if err == io.EOF {
-					break
-				}
-				log.Fatalf("reading stderr %v", err)
-			}
-
-			if n > 0 {
-				d := string(buf[:n])
-
-				// only required if we want to see the emulator output
-				log.Printf("%s", d)
-
-				// checking for the message that it's started
-				if strings.Contains(d, "Dev App Server is now running") {
-					wg.Done()
-					return
-				}
-
-				// and capturing the FIRESTORE_EMULATOR_HOST value to set
-				pos := strings.Index(d, FirestoreEmulatorHost+"=")
-				if pos > 0 {
-					host := d[pos+len(FirestoreEmulatorHost)+1 : len(d)-2]
-					log.Println("HOST: " + host)
-					err := os.Setenv(FirestoreEmulatorHost, host)
-					if err != nil {
-						fmt.Println("ERROR")
-						log.Fatalf("setting env var %v", err)
-					}
-				}
-			}
-		}
-	}()
-	// wait until the running message has been received
-	wg.Wait()
-	return func(result *int) {
-		// run command to kill firestore emulator, which is running on port 8080
-		err = exec.Command("powershell", "taskkill /PID $(netstat -ano | findstr.exe 8080 | grep LISTENING | awk '{print $5}' ) /F").Run()
-		if err != nil {
-			log.Fatal("ERROR ON KILLING " + err.Error())
-		}
-		os.Exit(*result)
-	}
-}
-
-const FirestoreEmulatorHost = "FIRESTORE_EMULATOR_HOST"
