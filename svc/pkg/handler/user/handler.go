a35c026df071ab03a4970a61222369239a1ed32a
Shion Ichikawa <shion1305@gmail.com>
2023-01-02 08:51:35 +0900

    ✨ (handler)InfoUpdateエンドポイントのhandlerを追加

--- svc/pkg/handler/user/handler.go
+++ svc/pkg/handler/user/handler.go
@@ -3,34 +3,69 @@ package user
 import (
 	"errors"
 	"github.com/gin-gonic/gin"
+	"ynufes-mypage-backend/svc/pkg/domain/command"
 	"ynufes-mypage-backend/svc/pkg/domain/model/user"
 	"ynufes-mypage-backend/svc/pkg/domain/query"
 	"ynufes-mypage-backend/svc/pkg/middleware"
 	"ynufes-mypage-backend/svc/pkg/registry"
+	"ynufes-mypage-backend/svc/pkg/schema"
 	uc "ynufes-mypage-backend/svc/pkg/uc/user"
 )
 
 type User struct {
-	userQ  query.User
-	infoUC uc.InfoUseCase
+	userQ        query.User
+	userC        command.User
+	infoUC       uc.InfoUseCase
+	infoUpdateUC uc.UserInfoUpdateUseCase
 }
 
 func NewUser(rgst registry.Registry) User {
 	return User{
-		userQ:  rgst.Repository().NewUserQuery(),
-		infoUC: uc.NewInfoUseCase(),
+		userQ:        rgst.Repository().NewUserQuery(),
+		userC:        rgst.Repository().NewUserCommand(),
+		infoUC:       uc.NewInfoUseCase(),
+		infoUpdateUC: uc.NewInfoUpdate(rgst),
 	}
 }
 
-func (ui User) InfoHandler() gin.HandlerFunc {
+func (uh User) InfoHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		u, exists := c.Get(middleware.UserContextKey)
 		u = u.(user.User)
 		if !exists || !(u).(user.User).IsValid() {
-			c.AbortWithError(500, errors.New("failed to retrieve user from context"))
+			_ = c.AbortWithError(500, errors.New("failed to retrieve user from context"))
 			return
 		}
-		_, _ = c.Writer.WriteString(ui.infoUC.Do(uc.InfoInput{User: u.(user.User)}).Response)
+		_, _ = c.Writer.WriteString(uh.infoUC.Do(uc.InfoInput{User: u.(user.User)}).Response)
+		c.Status(200)
+	}
+}
+
+func (uh User) InfoUpdateHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		uA, exists := c.Get(middleware.UserContextKey)
+		u, ok := uA.(user.User)
+		if !exists || !ok || u.IsValid() {
+			_ = c.AbortWithError(500, errors.New("failed to retrieve user from context"))
+			return
+		}
+		var req schema.InfoUpdateRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
+			_ = c.AbortWithError(400, err)
+			return
+		}
+		detail, err := req.ToUserDetail()
+		if err != nil {
+			_ = c.AbortWithError(400, err)
+			return
+		}
+		out := uh.infoUpdateUC.Do(uc.UserInfoUpdateInput{
+			OldUser:   &u,
+			NewDetail: *detail,
+		})
+		if out.Error != nil {
+			_ = c.AbortWithError(500, out.Error)
+		}
 		c.Status(200)
 	}
 }
