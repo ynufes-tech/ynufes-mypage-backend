2afcc20dabeeba2c072725dfb832082a91f7dd13
Shion Ichikawa <shion1305@gmail.com>
2023-01-02 08:45:17 +0900

    ✨ (model)UserDetailにvalidationメゾットの追加

--- svc/pkg/domain/model/user/detail.go
+++ svc/pkg/domain/model/user/detail.go
@@ -20,3 +20,20 @@ type (
 	}
 	StudentID string
 )
+
+// IsValid TODO: implement validation for StudentID
+func (s StudentID) IsValid() bool {
+	return s != ""
+}
+
+func (e Email) IsValid() bool {
+	return e != ""
+}
+
+func (n Name) HasAllValue() bool {
+	return n.FirstName != "" && n.LastName != "" && n.FirstNameKana != "" && n.LastNameKana != ""
+}
+
+func (d Detail) MeetsBasicRequirement() bool {
+	return d.Name.HasAllValue() && d.StudentID.IsValid() && d.Email.IsValid()
+}
