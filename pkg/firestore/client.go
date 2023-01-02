d1ab1771006d6ff9491c3f11ece83eae7b4e4ba4
Shion Ichikawa <shion1305@gmail.com>
2023-01-02 16:26:54 +0900

    💡 (pkg/firestore)テストモードに対応

--- pkg/firestore/client.go
+++ pkg/firestore/client.go
@@ -4,6 +4,7 @@ import (
 	"cloud.google.com/go/firestore"
 	"context"
 	"google.golang.org/api/option"
+	"log"
 	"os"
 	"ynufes-mypage-backend/pkg/setting"
 )
@@ -16,11 +17,15 @@ func New() *firestore.Client {
 	if client == nil {
 		ctx := context.Background()
 		config := setting.Get()
+		if config.Infrastructure.Firestore.JsonCredentialFile == "TESTING" {
+			client, _ = firestore.NewClient(ctx, "ynufes-mypage")
+			return client
+		}
 		data, err := os.ReadFile(config.Infrastructure.Firestore.JsonCredentialFile)
 		options := option.WithCredentialsJSON(data)
 		client, err = firestore.NewClient(ctx, config.Infrastructure.Firestore.ProjectID, options)
 		if err != nil {
-			panic(err)
+			log.Fatalf("firebase.NewClient err: %v", err)
 		}
 	}
 	return client
