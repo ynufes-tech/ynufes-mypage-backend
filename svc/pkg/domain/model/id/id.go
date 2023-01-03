ff69e441690fef49bee298f3349f8b7340a172a4
Shion Ichikawa <shion1305@gmail.com>
2023-01-03 23:59:51 +0900

    🎨 (model)IDモデルへImportIDメゾットの追加

--- svc/pkg/domain/model/id/id.go
+++ svc/pkg/domain/model/id/id.go
@@ -1,6 +1,14 @@
 package id
 
+import (
+	"ynufes-mypage-backend/pkg/identity"
+)
+
 type ID interface {
 	ExportID() string
 	HasValue() bool
+}
+
+func ImportID(id string) (ID, error) {
+	return identity.ImportID(id)
 }
