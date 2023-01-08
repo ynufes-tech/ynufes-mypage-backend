751760e3358171cc1621d11f45d6c766f60a0702
Shion Ichikawa <shion1305@gmail.com>
2023-01-08 13:51:41 +0900

    🎨 (registry)serviceにIDManagerを追加

--- svc/pkg/registry/service.go
+++ svc/pkg/registry/service.go
@@ -1,9 +1,11 @@
 package registry
 
 import (
+	"ynufes-mypage-backend/pkg/identity"
 	linePkg "ynufes-mypage-backend/pkg/line"
 	"ynufes-mypage-backend/pkg/setting"
 	lineDomain "ynufes-mypage-backend/svc/pkg/domain/service/line"
+	utilSVC "ynufes-mypage-backend/svc/pkg/domain/service/util"
 )
 
 type Service struct{}
@@ -15,4 +17,8 @@ func NewService() Service {
 func (s Service) NewLineAuthVerifier() lineDomain.AuthVerifier {
 	config := setting.Get()
 	return linePkg.NewAuthVerifier(config.ThirdParty.LineLogin.CallbackURI, config.ThirdParty.LineLogin.ClientID, config.ThirdParty.LineLogin.ClientSecret)
+}
+
+func (s Service) NewIDManager() utilSVC.IDManager {
+	return identity.NewIDManager()
 }
