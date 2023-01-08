adc113f1536f89e9872a8d612d0918d0ade53109
Shion Ichikawa <shion1305@gmail.com>
2023-01-08 22:15:27 +0900

    ✨ (reader)event

---
+++ svc/pkg/infra/reader/event.go
@@ -0,0 +1,41 @@
+package reader
+
+import (
+	"cloud.google.com/go/firestore"
+	"context"
+	"fmt"
+	"ynufes-mypage-backend/pkg/identity"
+	"ynufes-mypage-backend/svc/pkg/domain/model/event"
+	"ynufes-mypage-backend/svc/pkg/domain/service/util"
+	entity "ynufes-mypage-backend/svc/pkg/infra/entity/event"
+)
+
+type Event struct {
+	collection *firestore.CollectionRef
+	idManager  util.IDManager
+}
+
+func NewEvent(c *firestore.Client) Event {
+	return Event{
+		collection: c.Collection("events"),
+		idManager:  identity.NewIDManager(),
+	}
+}
+
+func (e Event) GetByID(ctx context.Context, id event.ID) (model *event.Event, err error) {
+	var eventEntity entity.Event
+	snap, err := e.collection.Doc(id.ExportID()).Get(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get event doc: %w", err)
+	}
+	err = snap.DataTo(&eventEntity)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode event doc into entity: %w", err)
+	}
+	eventEntity.ID = id
+	model, err = eventEntity.ToModel()
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert event entity to model: %w", err)
+	}
+	return model, nil
+}
