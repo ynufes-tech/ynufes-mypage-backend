64dd7fc00dfdbd2eb98f2bfaa2f00427fb72a2d7
Shion Ichikawa <shion1305@gmail.com>
2023-01-09 01:29:53 +0900

    🐛 (query)Orgクエリの修正, snowflake->org.ID +ctx

--- svc/pkg/domain/query/org.go
+++ svc/pkg/domain/query/org.go
@@ -1,10 +1,10 @@
 package query
 
 import (
-	"ynufes-mypage-backend/pkg/snowflake"
+	"context"
 	"ynufes-mypage-backend/svc/pkg/domain/model/org"
 )
 
 type Org interface {
-	GetByID(id snowflake.Snowflake) (*org.Org, error)
+	GetByID(ctx context.Context, id org.ID) (*org.Org, error)
 }
