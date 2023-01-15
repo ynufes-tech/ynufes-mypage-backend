7ac8956b99a0d112c3eab2391f26b14d063e7ca5
Shion Ichikawa <shion1305@gmail.com>
2023-01-15 22:25:07 +0900

    🐛 (query)Eventの修正

--- svc/pkg/domain/query/event.go
+++ svc/pkg/domain/query/event.go
@@ -1,10 +1,9 @@
 package query
 
 import (
-	"ynufes-mypage-backend/pkg/snowflake"
 	"ynufes-mypage-backend/svc/pkg/domain/model/event"
 )
 
 type Event interface {
-	GetByID(id snowflake.Snowflake) (*event.Event, error)
+	GetByID(id event.ID) (*event.Event, error)
 }
