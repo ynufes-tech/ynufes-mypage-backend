2d1f7c461a45a5332afa9a82eb2e3dbdcd7cd7d9
Shion Ichikawa <shion1305@gmail.com>
2023-01-17 05:06:08 +0900

    🐛 (entity)Form/Questionのfirestore連携,Properties修正

--- svc/pkg/infra/entity/form/aggregate.go
+++ svc/pkg/infra/entity/form/aggregate.go
@@ -2,17 +2,17 @@ package entity
 
 type (
 	Form struct {
-		ID          int64               `firestore:"id"`
+		ID          string              `firestore:"-"`
 		Title       string              `firestore:"title"`
 		Summary     string              `firestore:"summary"`
 		Description string              `firestore:"description"`
 		Questions   map[string]Question `firestore:"questions"`
 	}
 	Question struct {
-		ID           string                 `firestore:"question_id"`
-		QuestionText string                 `firestore:"question"`
-		QuestionType int                    `firestore:"question_type"`
-		QuestionData map[string]interface{} `firestore:"question_data"`
-		Order        int                    `firestore:"order"`
+		ID           string      `firestore:"question_id"`
+		QuestionText string      `firestore:"question"`
+		QuestionType int         `firestore:"question_type"`
+		Properties   interface{} `firestore:"properties"`
+		Order        int         `firestore:"order"`
 	}
 )
