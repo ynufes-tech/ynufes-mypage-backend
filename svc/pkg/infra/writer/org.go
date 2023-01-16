07ddb769695b7f20a5101afd88f6ff3ac77f49c0
Shion Ichikawa <shion1305@gmail.com>
2023-01-17 01:19:07 +0900

    ✨ (writer)Org Create

---
+++ svc/pkg/infra/writer/org.go
@@ -0,0 +1,43 @@
+package writer
+
+import (
+	"cloud.google.com/go/firestore"
+	"context"
+	"fmt"
+	"log"
+	"ynufes-mypage-backend/pkg/identity"
+	"ynufes-mypage-backend/svc/pkg/domain/model/org"
+	entity "ynufes-mypage-backend/svc/pkg/infra/entity/org"
+)
+
+type Org struct {
+	collection *firestore.CollectionRef
+}
+
+func NewOrg(c *firestore.Client) Org {
+	return Org{
+		collection: c.Collection("orgs"),
+	}
+}
+
+func (o Org) Create(ctx context.Context, org *org.Org) error {
+	memberE := make([]int64, len(org.Members))
+	for i := range org.Members {
+		memberE[i] = org.Members[i].GetValue()
+	}
+	id := identity.IssueID()
+	e := entity.Org{
+		EventID:   org.Event.ID.GetValue(),
+		EventName: org.Event.Name,
+		Name:      org.Name,
+		Members:   memberE,
+		IsOpen:    org.IsOpen,
+	}
+	_, err := o.collection.Doc(id.ExportID()).Create(ctx, e)
+	if err != nil {
+		log.Printf("Failed to create org: %v", err)
+		return fmt.Errorf("failed to create org: %w", err)
+	}
+	org.ID = id
+	return nil
+}
