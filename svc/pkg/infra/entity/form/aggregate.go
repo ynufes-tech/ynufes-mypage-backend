edbc6dcdbc67f7aa12b035e42c1990bb202b9335
Shion Ichikawa <shion1305@gmail.com>
2023-01-17 06:26:25 +0900

    ✨ (entity)Form ToModel()

--- svc/pkg/infra/entity/form/aggregate.go
+++ svc/pkg/infra/entity/form/aggregate.go
@@ -1,5 +1,11 @@
 package entity
 
+import (
+	"fmt"
+	"ynufes-mypage-backend/pkg/identity"
+	"ynufes-mypage-backend/svc/pkg/domain/model/form"
+)
+
 type (
 	Form struct {
 		ID          string              `firestore:"-"`
@@ -16,3 +22,35 @@ type (
 		Order        int         `firestore:"order"`
 	}
 )
+
+func (f Form) ToModel() (*form.Form, error) {
+	fid, err := identity.ImportID(f.ID)
+	if err != nil {
+		return nil, err
+	}
+	questions := make(map[form.QID]form.Question)
+	for _, v := range f.Questions {
+		qid, err := identity.ImportID(v.ID)
+		if err != nil {
+			return nil, err
+		}
+		properties, err := importQuestionProperties(form.QuestionType(v.QuestionType), v.Properties)
+		if err != nil {
+			return nil, fmt.Errorf("failed to import question properties in ToModel(): %w", err)
+		}
+		questions[qid] = form.Question{
+			ID:           qid,
+			Type:         form.QuestionType(v.QuestionType),
+			QuestionText: v.QuestionText,
+			Order:        0,
+			Properties:   *properties,
+		}
+	}
+	return &form.Form{
+		ID:          fid,
+		Title:       f.Title,
+		Summary:     f.Summary,
+		Description: f.Description,
+		Questions:   questions,
+	}, nil
+}
