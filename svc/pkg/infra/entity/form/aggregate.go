08ba7da6a24da1c2ec968e1f6dc5984324f0ebf1
Shion Ichikawa <shion1305@gmail.com>
2023-01-17 04:48:31 +0900

    ✨ (entity)Form

---
+++ svc/pkg/infra/entity/form/aggregate.go
@@ -0,0 +1,17 @@
+package entity
+
+type (
+	Form struct {
+		ID          int64               `firestore:"id"`
+		Title       string              `firestore:"title"`
+		Summary     string              `firestore:"summary"`
+		Description string              `firestore:"description"`
+		Questions   map[string]Question `firestore:"questions"`
+	}
+	Question struct {
+		ID           string                 `firestore:"question_id"`
+		QuestionText string                 `firestore:"question"`
+		QuestionType int                    `firestore:"question_type"`
+		QuestionData map[string]interface{} `firestore:"question_data"`
+	}
+)
