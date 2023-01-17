c43c204b20d7eef64b712f43b17c81b44c05622a
Shion Ichikawa <shion1305@gmail.com>
2023-01-17 23:28:01 +0900

    ✨ (model)Question, QuestionBasic, CheckboxQuestion

--- svc/pkg/domain/model/form/question.go
+++ svc/pkg/domain/model/form/question.go
@@ -11,13 +11,47 @@ const (
 )
 
 type (
-	Question struct {
-		ID           QID
-		Type         QuestionType
-		QuestionText string
-		Order        int
-		Properties   QuestionProperties
+	Question interface {
+		ID() QID
+		Type() QuestionType
+		Text() string
+		Order() int
 	}
 	QuestionType int
 	QID          util.ID
+
+	QuestionBasic struct {
+		QID           QID
+		QuestionText  string
+		QuestionOrder int
+	}
 )
+
+func (q QuestionBasic) ID() QID {
+	return q.QID
+}
+
+func (q QuestionBasic) Order() int {
+	return q.QuestionOrder
+}
+
+func (q QuestionBasic) Text() string {
+	return q.QuestionText
+}
+
+type (
+	CheckboxQuestion struct {
+		QuestionBasic
+		QID     QID
+		Options []CheckboxOption
+	}
+	CheckboxOption struct {
+		ID    util.ID
+		Text  string
+		Order int
+	}
+)
+
+func (q CheckboxQuestion) Type() QuestionType {
+	return CheckBox
+}
