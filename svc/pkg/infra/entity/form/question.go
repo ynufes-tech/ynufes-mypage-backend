106c11e12c31df87f9dffd99de9f5473136dfb27
Shion Ichikawa <shion1305@gmail.com>
2023-01-18 02:51:16 +0900

    ✨ (entity)ImportCheckboxQuestion

--- svc/pkg/infra/entity/form/question.go
+++ svc/pkg/infra/entity/form/question.go
@@ -1,6 +1,7 @@
 package entity
 
 import (
+	"errors"
 	"fmt"
 	"ynufes-mypage-backend/pkg/identity"
 	"ynufes-mypage-backend/svc/pkg/domain/model/form"
@@ -24,10 +25,41 @@ func ImportQuestion(q Question) (form.Question, error) {
 	}
 }
 
-func ImportCheckboxQuestion(q Question, basic form.QuestionBasic) (form.CheckboxQuestion, error) {
-	// TODO: implement Options converter
-	return form.CheckboxQuestion{
+func ImportCheckboxQuestion(q Question, basic form.QuestionBasic) (*form.CheckboxQuestion, error) {
+	var options []form.CheckboxOption
+	for k := range q.Properties {
+		id, err := identity.ImportID(k)
+		if err != nil {
+			return nil, fmt.Errorf("failed to import option id: %w", err)
+		}
+		p, ok := q.Properties[k].(map[string]interface{})
+		if !ok {
+			return nil, errors.New("invalid option property")
+		}
+		text, ok := p["text"]
+		if !ok {
+			return nil, errors.New("invalid option property")
+		}
+		textStr, ok := text.(string)
+		if !ok {
+			return nil, errors.New("invalid option property")
+		}
+		order, ok := p["order"]
+		if !ok {
+			return nil, errors.New("invalid option property")
+		}
+		orderInt, ok := order.(int)
+		if !ok {
+			return nil, errors.New("invalid option property")
+		}
+		options = append(options, form.CheckboxOption{
+			ID:    id,
+			Text:  textStr,
+			Order: orderInt,
+		})
+	}
+	return &form.CheckboxQuestion{
 		QuestionBasic: basic,
-		Options:       nil,
+		Options:       options,
 	}, nil
 }
