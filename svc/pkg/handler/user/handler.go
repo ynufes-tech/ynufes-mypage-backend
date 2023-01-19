ccba12aca365249943b11742661b3209210f6bb3
Shion Ichikawa <shion1305@gmail.com>
2023-01-19 11:31:26 +0900

    🎨 (uc,handler/user)infoUCの廃止,handlerに統合

--- svc/pkg/handler/user/handler.go
+++ svc/pkg/handler/user/handler.go
@@ -1,21 +1,22 @@
 package user
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"ynufes-mypage-backend/svc/pkg/domain/command"
 	"ynufes-mypage-backend/svc/pkg/domain/model/user"
 	"ynufes-mypage-backend/svc/pkg/domain/query"
 	"ynufes-mypage-backend/svc/pkg/middleware"
 	"ynufes-mypage-backend/svc/pkg/registry"
-	"ynufes-mypage-backend/svc/pkg/schema"
+	userSchema "ynufes-mypage-backend/svc/pkg/schema/user"
 	uc "ynufes-mypage-backend/svc/pkg/uc/user"
 )
 
 type User struct {
 	userQ        query.User
 	userC        command.User
-	infoUC       uc.InfoUseCase
 	infoUpdateUC uc.UserInfoUpdateUseCase
 }
 
@@ -23,20 +24,31 @@ func NewUser(rgst registry.Registry) User {
 	return User{
 		userQ:        rgst.Repository().NewUserQuery(),
 		userC:        rgst.Repository().NewUserCommand(),
-		infoUC:       uc.NewInfoUseCase(),
 		infoUpdateUC: uc.NewInfoUpdate(rgst),
 	}
 }
 
 func (uh User) InfoHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		u, exists := c.Get(middleware.UserContextKey)
-		u = u.(user.User)
-		if !exists || !(u).(user.User).IsValid() {
+		uAny, exists := c.Get(middleware.UserContextKey)
+		if !exists || !(uAny).(user.User).IsValid() {
 			_ = c.AbortWithError(500, errors.New("failed to retrieve user from context"))
 			return
 		}
-		_, _ = c.Writer.WriteString(uh.infoUC.Do(uc.InfoInput{User: u.(user.User)}).Response)
+		u := uAny.(user.User)
+		output := userSchema.InfoResponse{
+			NameFirst:       u.Detail.Name.FirstName,
+			NameLast:        u.Detail.Name.LastName,
+			Type:            int(u.Detail.Type),
+			ProfileImageURL: string(u.Line.LineProfilePictureURL),
+			Status:          int(u.Status),
+		}
+		j, err := json.Marshal(output)
+		if err != nil {
+			_ = c.AbortWithError(500, fmt.Errorf("failed to marshal output: %w", err))
+			return
+		}
+		_, _ = c.Writer.WriteString(string(j))
 		c.Status(200)
 	}
 }
