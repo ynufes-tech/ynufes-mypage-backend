9a42c5e1caeec8e663100fc29b66733b1ce0b557
Shion Ichikawa <shion1305@gmail.com>
2023-02-11 20:48:39 +0900

    ✨ (writer/org)Set, UpdateMembers, UpdateIsOpen

--- svc/pkg/infra/writer/org.go
+++ svc/pkg/infra/writer/org.go
@@ -41,3 +41,51 @@ func (o Org) Create(ctx context.Context, org *org.Org) error {
 	org.AssignID(org.ID)
 	return nil
 }
+
+func (o Org) Set(ctx context.Context, org org.Org) error {
+	memberE := make([]int64, len(org.Members))
+	e := entity.Org{
+		EventID:   org.Event.ID.GetValue(),
+		EventName: org.Event.Name,
+		Name:      org.Name,
+		Members:   memberE,
+		IsOpen:    org.IsOpen,
+	}
+	for i := range org.Members {
+		memberE[i] = org.Members[i].GetValue()
+	}
+	_, err := o.collection.Doc(org.ID.ExportID()).Set(ctx, e)
+	if err != nil {
+		log.Printf("Failed to update org: %v", err)
+		return fmt.Errorf("failed to update org: %w", err)
+	}
+	return nil
+}
+
+func (o Org) UpdateMembers(ctx context.Context, org org.Org) error {
+	memberE := make([]int64, len(org.Members))
+	for i := range org.Members {
+		memberE[i] = org.Members[i].GetValue()
+	}
+	_, err := o.collection.Doc(org.ID.ExportID()).Update(ctx,
+		[]firestore.Update{
+			{Path: "member_ids", Value: memberE},
+		})
+	if err != nil {
+		log.Printf("Failed to update org members: %v", err)
+		return fmt.Errorf("failed to update org members: %w", err)
+	}
+	return nil
+}
+
+func (o Org) UpdateIsOpen(ctx context.Context, org org.Org) error {
+	_, err := o.collection.Doc(org.ID.ExportID()).Update(ctx,
+		[]firestore.Update{
+			{Path: "is_open", Value: org.IsOpen},
+		})
+	if err != nil {
+		log.Printf("Failed to update org is_open: %v", err)
+		return fmt.Errorf("failed to update org is_open: %w", err)
+	}
+	return nil
+}
