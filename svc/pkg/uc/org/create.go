1800df6af5cce4ee9ac446ca7eaebb979d8820e4
Shion Ichikawa <shion1305@gmail.com>
2023-02-12 20:41:47 +0900

    ✨ (uc)CreateOrgUseCase

---
+++ svc/pkg/uc/org/create.go
@@ -0,0 +1,53 @@
+package org
+
+import (
+	"context"
+	"ynufes-mypage-backend/svc/pkg/domain/command"
+	"ynufes-mypage-backend/svc/pkg/domain/model/event"
+	"ynufes-mypage-backend/svc/pkg/domain/model/org"
+	"ynufes-mypage-backend/svc/pkg/domain/query"
+	"ynufes-mypage-backend/svc/pkg/registry"
+)
+
+type CreateOrgUseCase struct {
+	orgC   command.Org
+	eventQ query.Event
+}
+
+type CreateOrgInput struct {
+	Ctx     context.Context
+	EventID event.ID
+	OrgName string
+	IsOpen  bool
+}
+
+type CreateOrgOutput struct {
+	Org org.Org
+}
+
+func NewCreateOrg(rgst registry.Registry) *CreateOrgUseCase {
+	return &CreateOrgUseCase{
+		orgC:   rgst.Repository().NewOrgCommand(),
+		eventQ: rgst.Repository().NewEventQuery(),
+	}
+}
+
+func (uc CreateOrgUseCase) Do(ipt CreateOrgInput) (*CreateOrgOutput, error) {
+	e, err := uc.eventQ.GetByID(ipt.Ctx, ipt.EventID)
+	if err != nil {
+		return nil, err
+	}
+	o := org.Org{
+		Event:   *e,
+		Name:    ipt.OrgName,
+		Members: nil,
+		IsOpen:  ipt.IsOpen,
+	}
+	err = uc.orgC.Create(ipt.Ctx, &o)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateOrgOutput{
+		Org: o,
+	}, nil
+}
