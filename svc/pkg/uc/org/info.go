e12920e15411bfa4465b3448a63576c68df96b8e
Shion Ichikawa <shion1305@gmail.com>
2023-02-12 20:53:16 +0900

    ✨ (uc/org)InfoUseCase

---
+++ svc/pkg/uc/org/info.go
@@ -0,0 +1,37 @@
+package org
+
+import (
+	"context"
+	"ynufes-mypage-backend/svc/pkg/domain/model/org"
+	"ynufes-mypage-backend/svc/pkg/domain/query"
+	"ynufes-mypage-backend/svc/pkg/registry"
+)
+
+type InfoUseCase struct {
+	orgQ query.Org
+}
+
+type InfoInput struct {
+	Ctx context.Context
+	ID  org.ID
+}
+
+type InfoOutput struct {
+	Org org.Org
+}
+
+func NewInfo(rgst registry.Registry) InfoUseCase {
+	return InfoUseCase{
+		orgQ: rgst.Repository().NewOrgQuery(),
+	}
+}
+
+func (uc InfoUseCase) Do(ipt InfoInput) (opt *InfoOutput, err error) {
+	o, err := uc.orgQ.GetByID(ipt.Ctx, ipt.ID)
+	if err != nil {
+		return nil, err
+	}
+	return &InfoOutput{
+		Org: *o,
+	}, nil
+}
