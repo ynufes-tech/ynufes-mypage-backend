885c329689a40aecf33d049fb4f7aed4eace6e3f
Shion Ichikawa <shion1305@gmail.com>
2023-02-13 02:03:42 +0900

    ✨ (uc/event)CreateUC

--- svc/pkg/exception/error.go
+++ svc/pkg/exception/error.go
@@ -3,6 +3,7 @@ package exception
 import "errors"
 
 var (
-	ErrorInvalidHeader = errors.New("INVALID Authorization Header")
-	ErrInvalidJWT      = errors.New("INVALID JWT")
+	ErrorInvalidHeader   = errors.New("INVALID Authorization Header")
+	ErrInvalidJWT        = errors.New("INVALID JWT")
+	ErrIDAlreadyAssigned = errors.New("ID ALREADY ASSIGNED")
 )
