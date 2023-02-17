1755d83552e3bd18b50a26c80ef9c4f32dbb508a
Shion Ichikawa <shion1305@gmail.com>
2023-02-17 13:24:58 +0900

    ✨ (model/user)TypeにMemberの追加

--- svc/pkg/domain/model/user/type.go
+++ svc/pkg/domain/model/user/type.go
@@ -6,12 +6,15 @@ type Type int
 
 const (
 	TypeNormal Type = 1
+	TypeMember Type = 2
 )
 
 func NewType(t int) (Type, error) {
-	switch t {
-	case int(TypeNormal):
+	switch Type(t) {
+	case TypeNormal:
 		return TypeNormal, nil
+	case TypeMember:
+		return TypeMember, nil
 	default:
 		return -1, errors.New("USER TYPE VALUE IS INVALID")
 	}
