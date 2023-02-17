113fe6ab4eea2bccab8daf02373462f7ccc296c0
Shion Ichikawa <shion1305@gmail.com>
2023-02-17 09:37:29 +0900

    ✨ (model)NewRoleLevelメゾット

--- svc/pkg/domain/model/user/agent.go
+++ svc/pkg/domain/model/user/agent.go
@@ -3,6 +3,7 @@ package user
 import (
 	"time"
 	"ynufes-mypage-backend/svc/pkg/domain/model/util"
+	"ynufes-mypage-backend/svc/pkg/exception"
 )
 
 type (
@@ -20,6 +21,19 @@ type (
 
 const (
 	Viewer  RoleLevel = 1
-	Editor            = 2
-	Manager           = 3
+	Editor  RoleLevel = 2
+	Manager RoleLevel = 3
 )
+
+func NewRoleLevel(level int) (RoleLevel, error) {
+	switch RoleLevel(level) {
+	case Viewer:
+		return Viewer, nil
+	case Editor:
+		return Editor, nil
+	case Manager:
+		return Manager, nil
+	default:
+		return 0, exception.ErrInvalidRoleLevel
+	}
+}
