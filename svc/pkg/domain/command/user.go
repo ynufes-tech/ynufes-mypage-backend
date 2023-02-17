4d28eae6b88928905f08df34b8435a80948c938d
Shion Ichikawa <shion1305@gmail.com>
2023-02-17 22:19:46 +0900

    ✨ (command/user)UpdateAgent,UpdateAdmin

--- svc/pkg/domain/command/user.go
+++ svc/pkg/domain/command/user.go
@@ -9,6 +9,8 @@ type User interface {
 	Create(context.Context, user.User) error
 	UpdateLine(ctx context.Context, oldUser *user.User, update user.Line) error
 	UpdateUserDetail(ctx context.Context, oldUser *user.User, update user.Detail) error
+	UpdateAgent(ctx context.Context, oldUser *user.User, update user.Agent) error
+	UpdateAdmin(ctx context.Context, oldUser *user.User, update user.Admin) error
 	UpdateAll(context.Context, user.User) error
 	Delete(context.Context, user.User) error
 }
