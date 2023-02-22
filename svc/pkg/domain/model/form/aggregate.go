29c255791c1608f37462a287db51a29040acd226
Shion Ichikawa <shion1305@gmail.com>
2023-02-23 05:12:15 +0900

    ✨ (domain,entity/form)EventID,Rolesの追加

--- svc/pkg/domain/model/form/aggregate.go
+++ svc/pkg/domain/model/form/aggregate.go
@@ -1,14 +1,20 @@
 package form
 
-import "ynufes-mypage-backend/svc/pkg/domain/model/util"
+import (
+	"ynufes-mypage-backend/svc/pkg/domain/model/event"
+	"ynufes-mypage-backend/svc/pkg/domain/model/user"
+	"ynufes-mypage-backend/svc/pkg/domain/model/util"
+)
 
 type (
 	// Form TODO: Implement Sections field
 	Form struct {
 		ID          ID
+		EventID     event.ID
 		Title       string
 		Summary     string
 		Description string
+		Roles       []user.RoleID
 		//Sections    []SectionID
 	}
 
@@ -16,11 +22,13 @@ type (
 	//SectionID util.ID
 )
 
-func NewForm(id ID, title, summary, description string) *Form {
+func NewForm(id ID, eventID event.ID, title, summary, description string, roles []user.RoleID) *Form {
 	return &Form{
 		ID:          id,
+		EventID:     eventID,
 		Title:       title,
 		Summary:     summary,
 		Description: description,
+		Roles:       roles,
 	}
 }
--- svc/pkg/infra/entity/form/aggregate.go
+++ svc/pkg/infra/entity/form/aggregate.go
@@ -3,16 +3,19 @@ package entity
 import (
 	"ynufes-mypage-backend/pkg/identity"
 	"ynufes-mypage-backend/svc/pkg/domain/model/form"
+	"ynufes-mypage-backend/svc/pkg/domain/model/user"
 )
 
 const FormCollectionName = "Forms"
 
 type (
 	Form struct {
-		ID          string `firestore:"-"`
-		Title       string `firestore:"title"`
-		Summary     string `firestore:"summary"`
-		Description string `firestore:"description"`
+		ID          string  `firestore:"-"`
+		EventID     int64   `firestore:"event_id"`
+		Title       string  `firestore:"title"`
+		Summary     string  `firestore:"summary"`
+		Description string  `firestore:"description"`
+		Roles       []int64 `firestore:"roles"`
 	}
 )
 
@@ -21,5 +24,10 @@ func (f Form) ToModel() (*form.Form, error) {
 	if err != nil {
 		return nil, err
 	}
-	return form.NewForm(fid, f.Title, f.Summary, f.Description), nil
+	eID := identity.NewID(f.EventID)
+	roles := make([]user.RoleID, len(f.Roles))
+	for i, r := range f.Roles {
+		roles[i] = identity.NewID(r)
+	}
+	return form.NewForm(fid, eID, f.Title, f.Summary, f.Description, roles), nil
 }
