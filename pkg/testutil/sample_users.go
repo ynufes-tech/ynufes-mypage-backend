5abd534e5a1d168f391b540326dfb88652dca1fc
Shion Ichikawa <shion1305@gmail.com>
2023-03-03 00:25:50 +0900

    ✅ idの変更の反映

--- pkg/testutil/sample_users.go
+++ pkg/testutil/sample_users.go
@@ -2,13 +2,14 @@ package testutil
 
 import (
 	"ynufes-mypage-backend/pkg/identity"
+	"ynufes-mypage-backend/svc/pkg/domain/model/id"
 	"ynufes-mypage-backend/svc/pkg/domain/model/user"
 )
 
 func Users() []user.User {
 	return []user.User{
 		{
-			ID:     user.ID(identity.NewID(1234)),
+			ID:     id.UserID(identity.NewID(1234)),
 			Status: user.StatusNew,
 			Detail: user.Detail{
 				Name: user.Name{
@@ -29,7 +30,7 @@ func Users() []user.User {
 			},
 		},
 		{
-			ID:     user.ID(identity.NewID(12344)),
+			ID:     id.UserID(identity.NewID(12344)),
 			Status: user.StatusRegistered,
 			Detail: user.Detail{
 				Name: user.Name{
