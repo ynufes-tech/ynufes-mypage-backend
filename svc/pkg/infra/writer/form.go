c0e129eb4d85078e4adf646eab7d7df81d9fd816
Shion Ichikawa <shion1305@gmail.com>
2023-03-03 00:23:54 +0900

    💥 (writer/form)Sectionの独立に伴うForm変更

--- svc/pkg/infra/writer/form.go
+++ svc/pkg/infra/writer/form.go
@@ -18,47 +18,27 @@ func NewForm(client *firestore.Client) *Form {
 }
 
 func (f Form) Create(ctx context.Context, target form.Form) error {
-	sectionOrder := make([]int64, len(target.SectionIDs))
-	for i := range target.SectionIDs {
-		sectionOrder[i] = target.SectionIDs[i].GetValue()
+	sections := make([]int64, len(target.Sections))
+	for i := range target.Sections {
+		sections[i] = target.Sections[i].GetValue()
 	}
 
-	sections := make([]entity.Section, 0, len(target.Sections))
-	for id, section := range target.Sections {
-		qIDs := make([]int64, len(section.QuestionIDs))
-		for i := range section.QuestionIDs {
-			qIDs[i] = section.QuestionIDs[i].GetValue()
-		}
-		cCustoms := make(map[string]int64, len(section.ConditionCustoms))
-		for k, v := range section.ConditionCustoms {
-			cCustoms[k.ExportID()] = v.GetValue()
-		}
-		sections = append(sections, entity.NewSection(
-			id.GetValue(),
-			qIDs,
-			section.ConditionQuestion.GetValue(),
-			cCustoms,
-		))
-	}
-
-	formID := target.ID.ExportID()
 	var roles = make([]int64, len(target.Roles))
 	for i := 0; i < len(target.Roles); i++ {
 		roles[i] = target.Roles[i].GetValue()
 	}
 	e := entity.NewForm(
-		formID,
+		target.ID,
 		target.EventID.GetValue(),
 		target.Title,
 		target.Summary,
 		target.Description,
+		sections,
 		roles,
 		target.Deadline.UnixMilli(),
 		target.IsOpen,
-		sectionOrder,
-		sections,
 	)
-	_, err := f.collection.Doc(formID).Create(ctx, e)
+	_, err := f.collection.Doc(target.ID.ExportID()).Create(ctx, e)
 	if err != nil {
 		return err
 	}
@@ -66,46 +46,27 @@ func (f Form) Create(ctx context.Context, target form.Form) error {
 }
 
 func (f Form) Set(ctx context.Context, target form.Form) error {
-	formID := target.ID.ExportID()
+	sections := make([]int64, len(target.Sections))
+	for i := range target.Sections {
+		sections[i] = target.Sections[i].GetValue()
+	}
+
 	var roles = make([]int64, len(target.Roles))
 	for i := 0; i < len(target.Roles); i++ {
 		roles[i] = target.Roles[i].GetValue()
 	}
-	sectionOrder := make([]int64, len(target.SectionIDs))
-	for i := range target.SectionIDs {
-		sectionOrder[i] = target.SectionIDs[i].GetValue()
-	}
-
-	sections := make([]entity.Section, 0, len(target.Sections))
-	for id, section := range target.Sections {
-		qIDs := make([]int64, len(section.QuestionIDs))
-		for i := range section.QuestionIDs {
-			qIDs[i] = section.QuestionIDs[i].GetValue()
-		}
-		cCustoms := make(map[string]int64, len(section.ConditionCustoms))
-		for k, v := range section.ConditionCustoms {
-			cCustoms[k.ExportID()] = v.GetValue()
-		}
-		sections = append(sections, entity.NewSection(
-			id.GetValue(),
-			qIDs,
-			section.ConditionQuestion.GetValue(),
-			cCustoms,
-		))
-	}
 	e := entity.NewForm(
-		formID,
+		target.ID,
 		target.EventID.GetValue(),
 		target.Title,
 		target.Summary,
 		target.Description,
+		sections,
 		roles,
 		target.Deadline.UnixMilli(),
 		target.IsOpen,
-		sectionOrder,
-		sections,
 	)
-	_, err := f.collection.Doc(formID).Set(ctx, e)
+	_, err := f.collection.Doc(target.ID.ExportID()).Set(ctx, e)
 	if err != nil {
 		return err
 	}
