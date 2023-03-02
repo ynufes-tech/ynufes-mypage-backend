34665a64685c9ac89d17698204d10bbf68fc1d8e
Shion Ichikawa <shion1305@gmail.com>
2023-03-03 04:33:01 +0900

    ✨ (pkg/error)ErrIDAlreadyAssigned

--- svc/pkg/exception/error.go
+++ svc/pkg/exception/error.go
@@ -3,8 +3,9 @@ package exception
 import "errors"
 
 var (
-	ErrorInvalidHeader  = errors.New("INVALID Authorization Header")
-	ErrInvalidJWT       = errors.New("INVALID JWT")
-	ErrIDNotAssigned    = errors.New("ID NOT ASSIGNED")
-	ErrInvalidRoleLevel = errors.New("INVALID ROLE LEVEL")
+	ErrorInvalidHeader   = errors.New("INVALID Authorization Header")
+	ErrInvalidJWT        = errors.New("INVALID JWT")
+	ErrIDAlreadyAssigned = errors.New("ID ALREADY ASSIGNED")
+	ErrIDNotAssigned     = errors.New("ID NOT ASSIGNED")
+	ErrInvalidRoleLevel  = errors.New("INVALID ROLE LEVEL")
 )
