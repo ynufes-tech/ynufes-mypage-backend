bfabf85da5ce007d81eee10beeda995a2df824d0
Shion Ichikawa <shion1305@gmail.com>
2023-03-03 07:02:18 +0900

    🎨 (entity/user)UserDetailのjsonタグにomitemptyを指定

--- svc/pkg/infra/entity/user/details.go
+++ svc/pkg/infra/entity/user/details.go
@@ -3,13 +3,13 @@ package entity
 type (
 	// UserDetail StudentID 学部生は数字7桁, 大学院生は数字2桁+英数字2桁+数字3桁
 	UserDetail struct {
-		NameFirst     string `json:"detail-name_first"`
-		NameFirstKana string `json:"detail-name_first_kana"`
-		NameLast      string `json:"detail-name_last"`
-		NameLastKana  string `json:"detail-name_last_kana"`
-		Gender        int    `json:"detail-gender"`
-		StudentID     string `json:"detail-student_id"`
-		Email         string `json:"detail-email"`
-		Type          int    `json:"detail-type"`
+		NameFirst     string `json:"detail-name_first,omitempty"`
+		NameFirstKana string `json:"detail-name_first_kana,omitempty"`
+		NameLast      string `json:"detail-name_last,omitempty"`
+		NameLastKana  string `json:"detail-name_last_kana,omitempty"`
+		Gender        int    `json:"detail-gender,omitempty"`
+		StudentID     string `json:"detail-student_id,omitempty"`
+		Email         string `json:"detail-email,omitempty"`
+		Type          int    `json:"detail-type,omitempty"`
 	}
 )
