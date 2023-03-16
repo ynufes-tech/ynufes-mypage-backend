237aff05ed177641393c0b2f853c1af25d631bc2
Shion Ichikawa <shion1305@gmail.com>
2023-03-16 22:00:39 +0900

    ✨ (infra/line)

---
+++ svc/pkg/infra/reader/line.go
@@ -0,0 +1,65 @@
+package reader
+
+import (
+	"context"
+	"firebase.google.com/go/v4/db"
+	"fmt"
+	"ynufes-mypage-backend/pkg/firebase"
+	"ynufes-mypage-backend/svc/pkg/domain/model/id"
+	"ynufes-mypage-backend/svc/pkg/domain/model/line"
+	"ynufes-mypage-backend/svc/pkg/exception"
+	entity "ynufes-mypage-backend/svc/pkg/infra/entity/line"
+)
+
+type Line struct {
+	ref *db.Ref
+}
+
+func NewLine(fb firebase.Firebase) Line {
+	return Line{
+		ref: fb.Client(entity.LineRootName),
+	}
+}
+
+func (l Line) GetByUserID(ctx context.Context, userID id.UserID) (*line.LineUser, error) {
+	r, err := l.ref.OrderByChild("user_id").EqualTo(userID).GetOrdered(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if len(r) == 0 {
+		return nil, exception.ErrNotFound
+	}
+	if len(r) > 1 {
+		fmt.Printf("multiple line found with user id: %s", userID)
+	}
+	var lineEntity entity.Line
+	if err := r[0].Unmarshal(&lineEntity); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal line entity: %w", err)
+	}
+	lineEntity.LineServiceID = r[0].Key()
+	model, err := lineEntity.ToModel()
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert line entity to model: %w", err)
+	}
+	return model, nil
+}
+
+func (l Line) GetByLineServiceID(ctx context.Context, lineID line.LineServiceID) (*line.LineUser, error) {
+	r, err := l.ref.OrderByKey().EqualTo(lineID).GetOrdered(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if len(r) == 0 {
+		return nil, exception.ErrNotFound
+	}
+	var lineEntity entity.Line
+	if err := r[0].Unmarshal(&lineEntity); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal line entity: %w", err)
+	}
+	lineEntity.LineServiceID = string(lineID)
+	model, err := lineEntity.ToModel()
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert line entity to model: %w", err)
+	}
+	return model, nil
+}
