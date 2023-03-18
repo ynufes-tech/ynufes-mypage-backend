7a225353cfdd2661f77b4d3bbd5d689e7f1cc013
Shion Ichikawa <shion1305@gmail.com>
2023-03-19 06:26:40 +0900

    ✨ (pkg/gcs) Name(),Download()

--- pkg/gcs/object.go
+++ pkg/gcs/object.go
@@ -2,6 +2,8 @@ package gcs
 
 import (
 	"cloud.google.com/go/storage"
+	"context"
+	"io"
 	"time"
 )
 
@@ -12,6 +14,24 @@ type (
 	}
 	SignedObjectLink string
 )
+
+func (r ObjectRef) Name() string {
+	return r.objName
+}
+
+func (r ObjectRef) Download(ctx context.Context) ([]byte, error) {
+	reader, err := r.bucket.Object(r.objName).NewReader(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
 
 func (r ObjectRef) IssueLink() (SignedObjectLink, error) {
 	url, err := r.bucket.SignedURL(r.objName, &storage.SignedURLOptions{
