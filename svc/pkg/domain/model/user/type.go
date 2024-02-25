20c569cc4c5ed4384ddf53450f2dd091c74d624e
Shion Ichikawa <shion1305@gmail.com>
2024-02-25 21:56:00 +0900

    🔥 remove TypeMember

--- svc/pkg/domain/model/user/type.go
+++ svc/pkg/domain/model/user/type.go
@@ -2,19 +2,18 @@ package user
 
 import "errors"
 
+// Type represents the type of user.
+// This field is currently not used, but it is expected to be used in the future.
 type Type int
 
 const (
 	TypeNormal Type = 1
-	TypeMember Type = 2
 )
 
 func NewType(t int) (Type, error) {
 	switch Type(t) {
 	case TypeNormal:
 		return TypeNormal, nil
-	case TypeMember:
-		return TypeMember, nil
 	default:
 		return -1, errors.New("USER TYPE VALUE IS INVALID")
 	}
